bb8707b7451390792d3fb9723a3303171023a6b4
morvan <morvan@192.168.0.19>
2016-05-02 02:45:36 +0200

    ajout cptFaller

---
+++ src/game/tests/LevelTest.java
@@ -0,0 +1,35 @@
+package game.tests;
+
+import game.components.Level;
+import game.services.LevelService;
+import game.contracts.LevelContract;
+import game.contracts.PostconditionError;
+import game.contracts.PreconditionError;
+import game.contracts.InvariantError;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class LevelTest {
+	private LevelService level;
+	
+	@Before
+	public void beforeTests(){
+		level = new LevelContract(new Level());
+	}
+	
+	@Test
+	// OK
+	public void testNaturePre1_1() {
+		
+		try {
+			lc.init(MIN_LEVEL, MAX_LEVEL);
+		}
+		catch (PostconditionError e) {
+	
+		}
+	}
+}
